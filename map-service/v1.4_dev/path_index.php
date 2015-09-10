4e569c6e2e7da2b916f337608f62ae1db7db6261
Franck Theeten <franck.theeten@africamuseum.be>
2015-09-10 17:27:30 +0200

    Minor updates

--- map-service/v1.4_dev/path_index.php
+++ map-service/v1.4_dev/path_index.php
@@ -2,7 +2,7 @@
 	//these constants added by ftheeten 04/03/2010
 	
 
-	define("POSTGIS_CS", 'host=localhost port=5432 password=fv30714$A user=postgres dbname=edit_geo_mirror');
+	define("POSTGIS_CS", 'host=localhost port=5432 password=********* user=********** dbname=edit_geo_mirror');
 	define("URL_GEOSERVER", "http://edit.africamuseum.be/geoserver/wms");
 	define("URL_SITE", "http://edit.africamuseum.be");
 	define("V1_IMG","/var/www/synthesys/www/v1.4/img/");
